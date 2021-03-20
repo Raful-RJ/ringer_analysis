9e91e1dcbc98f841f545488ac06d61e9bda37ed8
Juan Lieber Marin <juan.lieber.marin@gmail.com>
2021-03-20 20:33:47 -0300

    update physval paths

--- 2020/collector_v1_mc16_13TeV_MC_selection/run_mc16_13TeV_Zrad_gammajet_collector.sh
+++ 2020/collector_v1_mc16_13TeV_MC_selection/run_mc16_13TeV_Zrad_gammajet_collector.sh
@@ -1,4 +1,4 @@
-BASEPATH_gammajet = /home/juan.marin/datasets/physval/signal/*
+BASEPATH_gammajet = /home/juan.marin/datasets/physval/user.jlieberm.mc16_13TeV.4231*/*.root
 
 prun_jobs.py -c "python3 job_collector.py --Zrad" -mt 40 -i BASEPATH_gammajet
 
