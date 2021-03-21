e4e1277ce06d9a9a6830db3b3108b5826a647ee9
Juan Lieber Marin <juan.lieber.marin@gmail.com>
2021-03-21 10:28:49 -0300

    fix small things

--- 2020/collector_v1_mc16_13TeV_MC_selection/run_mc16_13TeV_Zrad_dijet_collector.sh
+++ 2020/collector_v1_mc16_13TeV_MC_selection/run_mc16_13TeV_Zrad_dijet_collector.sh
@@ -1,5 +1,5 @@
-BASEPATH_dijet = /home/juan.marin/datasets/physval/user.jlieberm.mc16_13TeV.4233*/*.root
-prun_jobs.py -c "python3 job_collector.py --fakes" -mt 40 -i BASEPATH_dijet
+BASEPATH_dijet=/home/juan.marin/datasets/physval/user.jlieberm.mc16_13TeV.4233*/*.root
+prun_jobs.py -c "python3 job_collector.py --fakes" -mt 40 -i $BASEPATH_dijet
 
 
 mkdir dijetMC
--- 2020/collector_v1_mc16_13TeV_MC_selection/run_mc16_13TeV_Zrad_gammajet_collector.sh
+++ 2020/collector_v1_mc16_13TeV_MC_selection/run_mc16_13TeV_Zrad_gammajet_collector.sh
@@ -1,6 +1,6 @@
-BASEPATH_gammajet = /home/juan.marin/datasets/physval/user.jlieberm.mc16_13TeV.4231*/*.root
+BASEPATH_gammajet=/home/juan.marin/datasets/physval/user.jlieberm.mc16_13TeV.4231*/*.root
 
-prun_jobs.py -c "python3 job_collector.py --Zrad" -mt 40 -i BASEPATH_gammajet
+prun_jobs.py -c "python3 job_collector.py --Zrad" -mt 40 -i $BASEPATH_gammajet
 
 mkdir gammajetMC
 rm *.root
