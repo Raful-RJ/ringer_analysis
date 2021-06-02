f86f1b6a403b59e52526f76787957d934349164d
Juan Marin <juan.lieber.marin@gmail.com>
2021-06-02 10:35:14 -0300

    update tools

--- 2020/efficiency_v1_mc16_13TeV_MC_selection_dijet/plot_efficiency_mc16_13TeV_MC_selection_dijet.py
+++ 2020/efficiency_v1_mc16_13TeV_MC_selection_dijet/plot_efficiency_mc16_13TeV_MC_selection_dijet.py
@@ -1,10 +1,12 @@
 from Gaugi.messenger import LoggingLevel, Logger
 from EfficiencyTools import PlotProfiles
 from Gaugi.storage import  restoreStoreGate
-from Gaugi.monet import AtlasTemplate1, SetAtlasStyle, setLegend1
+from Gaugi.monet import AtlasTemplate1, SetAtlasStyle, setLegend1,  AddTexLabel
 from ROOT import gROOT, TLegend
 from ROOT import kBlack, kBlue, kGray, kOrange, kPink, kViolet, kRed, kCyan, kAzure
 from ROOT import kPlus, kCircle, kMultiply, kStar, kDot, kOpenTriangleUp, kFullTriangleDown, kFullSquare, kFullCircle
+from ROOT import kBlack, kBlue, kRed, kCircle, kFullCircle, kOpenSquare, kFullSquare, kOpenTriangleUp, kFullTriangleUp, kMultiply
+
 import argparse
 gROOT.SetBatch(True)
 
@@ -53,35 +55,38 @@ level     = args.level
 
 
 sg =  restoreStoreGate( inputFile )
-
-# triggers = [
-#                 "EMU_g20_tight_noringer_L1EM3",
-#                 "EMU_g20_medium_noringer_L1EM3",
-#                 "EMU_g20_loose_noringer_L1EM3",
-#                 "EMU_g15_tight_noringer_L1EM3",
-#                 "EMU_g15_medium_noringer_L1EM3",
-#                 "EMU_g15_loose_noringer_L1EM3",
-#                 "EMU_g5_tight_noringer_L1EM3",
-#                 "EMU_g5_medium_noringer_L1EM3",
-#                 "EMU_g5_loose_noringer_L1EM3",
-#                 ]
-
-
 triggers = [
-                "EMU_g20_loose_noringer_L1EM3",
-                "EMU_g20_loose_ringer_v1_L1EM3",
+            # "EMU_g20_tight_noringer_L1EM15VHI",
+            # "EMU_g20_tight_ringer_v1_L1EM15VHI",
+            # "EMU_g20_medium_noringer_L1EM15VHI",
+            # "EMU_g20_medium_ringer_v1_L1EM15VHI",
+            # "EMU_g20_loose_noringer_L1EM15VHI",
+            # "EMU_g20_loose_ringer_v1_L1EM15VHI",
+            "EMU_g35_tight_noringer_L1EM22VHI",
+            "EMU_g35_tight_ringer_v1_L1EM22VHI",
+            "EMU_g35_medium_noringer_L1EM22VHI",
+            "EMU_g35_medium_ringer_v1_L1EM22VHI",
+            "EMU_g35_loose_noringer_L1EM22VHI",
+            "EMU_g35_loose_ringer_v1_L1EM22VHI",
+            # "EMU_g25_tight_noringer_L1EM20VHI",
+            # "EMU_g25_tight_ringer_v1_L1EM20VHI",
+            # "EMU_g25_medium_noringer_L1EM20VHI",
+            # "EMU_g25_medium_ringer_v1_L1EM20VHI",
+            # "EMU_g25_loose_noringer_L1EM20VHI",
+            # "EMU_g25_loose_ringer_v1_L1EM20VHI",
+]
 
-                ]
+theseColors = [kBlack, kBlack, kRed, kRed, kBlue, kBlue]
+theseMarkers = [kCircle, kFullCircle, kOpenSquare, kFullSquare, kOpenTriangleUp, kFullTriangleUp]
+# theseMarkers = [kCircle, kMultiply, kOpenSquare, kMultiply, kOpenTriangleUp, kMultiply]
 
-theseColors = [i+1 for i in range(0,len(triggers))]
-theseMarkers = [i+4 for i in range(0,len(triggers))]
 theseTransColors = [i+1 for i in range(0,len(triggers))]
 eff_et  = [ sg.histogram( basepath+'/'+trigger+'/Efficiency/' + level + '/eff_et' ) for trigger in triggers ]
 eff_eta = [ sg.histogram( basepath+'/'+trigger+'/Efficiency/' + level + '/eff_eta' ) for trigger in triggers ]
 eff_phi = [ sg.histogram( basepath+'/'+trigger+'/Efficiency/' + level + '/eff_phi' ) for trigger in triggers ]
 eff_mu  = [ sg.histogram( basepath+'/'+trigger+'/Efficiency/' + level + '/eff_mu' ) for trigger in triggers ]
 
-efficiencyObjects = { 'E_T[GeV]'  : eff_et,
+efficiencyObjects = { '\rE_{T}[GeV]'   : eff_et,
                       '#eta'      : eff_eta, 
                       '#varphi'   : eff_phi, 
                       '<#mu>'     : eff_mu
@@ -90,16 +95,29 @@ for hist in eff_mu:
   hist.SetBins(9,0,40);  
 SetAtlasStyle()
 
-legend =TLegend()
+legend =TLegend(0.2,0.5,0.5,0.7)
+legends=[]
+for idx, hist in enumerate(eff_et):
+    temp_legend = triggers[idx].replace('EMU','HLT')
+    try:
+        legends.append(temp_legend.replace('_noringer',''))
+    except:
+        legends.append(temp_legend.replace('_ringer_v1',''))
+
 
 for idx, hist in enumerate(eff_et):
-  legend.AddEntry(hist, triggers[idx])
+  hist.SetMarkerStyle(theseMarkers[idx])
+  if 'ringer' in legends[idx]: continue
+  legend.AddEntry(hist, legends[idx].replace('EMU','HLT'),'l')
 
-for obj in efficiencyObjects:
-    eff_canvas = PlotProfiles( efficiencyObjects[obj], xlabel=obj, these_colors = theseColors, these_transcolors = theseTransColors, these_markers = theseMarkers)
+fileName = ['fakeEt', 'fakelEta', 'fakePhi', 'fakeMu']
+
+for idx, obj in enumerate(efficiencyObjects):
+    eff_canvas = PlotProfiles( efficiencyObjects[obj], xlabel=obj, these_colors = theseColors, these_transcolors = theseTransColors, these_markers = theseMarkers, ylabel='Trigger Background Efficiency'  ,y_axes_maximum=0.6)
     setLegend1(legend)
-    AtlasTemplate1(canvas = eff_canvas, dolumi=True, atlaslabel='Internal')
-    eff_canvas.SaveAs(level + '_' + obj + '.pdf')
+    AtlasTemplate1(canvas = eff_canvas, dolumi=True, atlaslabel='Internal, MC data 2016')
+    AddTexLabel(eff_canvas, 0.2, 0.75, 'close: with Ringer open: without Ringer', textsize=0.04)
+    eff_canvas.SaveAs(level + '_' + fileName[idx] + '.pdf')
 
 for trigger in triggers:
   plot_table( sg, mainLogger, trigger, basepath )
